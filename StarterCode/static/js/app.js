f62f8c9a47e377fdcc71efb8ddb796f8dd2448b3
abhijitpurru2 <68308831+abhijitpurru2@users.noreply.github.com>
2021-01-28 16:31:58 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,3 @@
-
 function dropdownCreator() {
     var dropdownMenu = d3.select("#selDataset");
     d3.json('samples.json').then((sampleData) => {
@@ -9,14 +8,42 @@ function dropdownCreator() {
                 .property("value", info)
         });
         metadataVisual(dataNames[0]);
+        chartsVisual(dataNames[0]);
+    });
+}
+
+function optionChanged(sampleNo) {
+    metadataVisual(sampleNo);
+    chartsVisual(sampleNo);
+}
+
+function chartsVisual(sampleNo){
+    d3.json("sample.json").then((data) => {
+        var samples = data.samples;
+        var info = samples.filter(sampleObject => sampleObject.id == sampleNo);
+
+        var otu_ids = info.otu_ids;
+        var otu_labels = info.otu_labels;
+        var sample_values = info.sample_values;
+
+        var barChart = [{
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h',
+            color: 'blue'
+        }];
+
+        Plotly.newPlot("bar", barChart);
     });
 }
 
 function metadataVisual(sampleNo){
-    d3.json("samples.json").then((data2) => {
+    d3.json("samples.json").then((data) => {
         var metadataD3 = d3.select("#sample-metadata");
         metadataD3.html("");
-        var metadataInfo = data2.metadata;
+        var metadataInfo = data.metadata;
         var info = metadataInfo.filter(sampleObject => sampleObject.id == sampleNo);
         Object.entries(info[0]).forEach(([key, value]) => {
             metadataD3.append("h6").text(`${key}: ${value}`);
@@ -24,16 +51,4 @@ function metadataVisual(sampleNo){
     });
 }
 
-function chart(sampleNo){
-
-}
-
 dropdownCreator();
-
-
-
-
-
-
-
-
